75385d64a96a43af630253ec361ed50666ec841f
Anthony Martino <anthonym225@gmail.com>
2019-04-09 14:39:06 -0400

    Added vertices and major axis information

--- Ellipse.py
+++ Ellipse.py
@@ -13,6 +13,7 @@ underY = float(input("What is the value under y?: "))
 
 
 if underX > underY:
+    majorAxis = "The Major Axis follows the X axis"
     a2 = underX
     b2 = underY
     a = sympy.sqrt(a2)
@@ -20,8 +21,12 @@ if underX > underY:
     c2 = a2 - b2
     c = sympy.sqrt(c2)
     foci = "The foci are at: ({}, {} + {}) and ({}, {} - {})".format(h, k, c, h, k, c)
+    vertices = "The vertices are at: ({}, {}) and ({}, {})".format(h + a, k, h - a, k)
+    print(majorAxis)
     print(foci)
+    print(vertices)
 else:
+    majorAxis = "The Major Axis follows the Y axis"
     a2 = underY
     b2 = underX
     a = sympy.sqrt(a2)
@@ -29,4 +34,7 @@ else:
     c2 = a2 - b2
     c = sympy.sqrt(c2)
     foci = "The foci are at: ({} + {}, {}) and ({} - {}, {})".format(h, c, k, h, c, k)
+    vertices = "The vertices are at: ({}, {}) and ({}, {})".format(h + a, k, h - a, k)
+    print(majorAxis)
     print(foci)
+    print(vertices)
